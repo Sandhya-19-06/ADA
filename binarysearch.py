f026e24bd616f395db41d9c9d53f732c8326cd6a
Sandhya <sandhyamandal2706@gmail.com>
2026-01-22 11:47:33 +0530

    initial commit

---
+++ binarysearch.py
@@ -0,0 +1,15 @@
+arr=list(map(int,input("enter sorted elements:").split()))
+key=int(input("enter element to search:"))
+low=0
+high=len(arr)-1
+while low<=high:
+    mid=(low+high)//2
+    if arr [mid]==key:
+        print("element found at position:",mid+1)
+        break
+    elif arr [mid]<key:
+        low=mid+1
+    else:
+        high=mid-1
+else:
+    print("elements not found")  
