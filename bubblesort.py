d8c89ab1ed03d0a083483e532ac7e29bba9b7a7b
Sandhya <sandhyamandal2706@gmail.com>
2026-01-22 11:48:26 +0530

    initial commit

---
+++ bubblesort.py
@@ -0,0 +1,7 @@
+n=int(input("enter number of elements:"))
+arr=list(map(int,input("enter elements:").split()))
+for i in range(n-1):
+    for j in range(n-i-1):
+        if arr[j]>arr[j+1]:
+         arr[j],arr[j+1]=arr[j+1],arr[j]
+print("sorted arr:,",arr)         
