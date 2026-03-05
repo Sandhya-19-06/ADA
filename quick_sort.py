5c8a987d329a42be9a1fecadf2edc38041f2971f
Sandhya <sandhyamandal2706@gmail.com>
2026-03-05 11:53:08 +0530

    initial commit

---
+++ quick_sort.py
@@ -0,0 +1,28 @@
+def Quick_sort(arr, low,high):
+    if low<high:
+        pivot=partition(arr,high,low)
+        Quick_sort(arr,low,pivot-1)
+        Quick_sort(arr,pivot+1,high)
+        
+def partition(arr,low,high):
+    p=arr[low]
+    i=low+1 
+    j=high
+    while True:
+        while i<=j and arr [i]<=p:
+            i+=1
+        while i<=j and arr[j]>=p:
+            j-=1
+        if i<=j:
+            arr[i], arr[j]=arr[j],arr[i] 
+        else:
+            break
+        arr[low],arr[j]=arr[j],arr[low]
+        return j
+arr=[4,5,6,7,1,8] 
+print(arr) 
+Quick_sort(arr,0,len(arr)-1)
+print(arr)         
+        
+            
+           
